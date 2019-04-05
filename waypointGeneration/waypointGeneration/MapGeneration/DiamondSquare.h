67f1eb8bdf6f654038cd350af2cdadf69e6b7723
Derp <derp@hotmail.com>
2019-04-05 09:58:35 +0200

    Removed DSA functions

--- waypointGeneration/waypointGeneration/MapGeneration/DiamondSquare.h
+++ waypointGeneration/waypointGeneration/MapGeneration/DiamondSquare.h
@@ -7,8 +7,6 @@ public:
 	~DiamondSquare();
 
 	std::vector<float> CreateDiamondSquare(int mapSize, int stepSize, float noise);
-	// Requires unified arraysize of all the arrays
-	std::vector<std::vector<float>> AdvancedCreateDiamondSquare(int mapSize[], int stepSize[], float noise[], int arraySize);
 
 private:
 	int m_mapSize;
@@ -21,16 +19,6 @@ private:
 	void _squareStep(int x, int z, int stepSize, int halfStep, float noise);
 	void _diamondSquareAlgorithm(int stepSize, float noise);
 
-	// Advanced diamond square algorithm creating different parts of the map with different values.
-	// The algorithm will create separate parts and then connect them.
-	// This makes it possible to have different map widths in the same map.
-	float _advancedGetValue(const std::vector<float> & map, int mapSize, int x, int z);
-	void _advancedSetValue(std::vector<float> & map, int mapSize, int x, int z, float value);
-	void _advancedDiamondStep(std::vector<float> & map, int mapSize, int x, int z, int stepSize, int halfStep, float noise);
-	void _advancedSquareStep(std::vector<float> & map, int mapSize, int x, int z, int stepSize, int halfStep, float noise);
-	void _advancedDiamondSquareAlgorithm(std::vector<std::vector<float>> & map, int mapSize, int stepSize, float noise);
-
 	// Smoothing
 	void _smoothValues(int filtersize); // filtersize must be 2^n + 1
-	void _advancedSmoothValues(std::vector<float> & map, int filtersize);
 };
